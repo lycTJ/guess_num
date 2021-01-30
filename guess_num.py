d3aee96638771f93aa9558bae7203c18b3fbcf9c
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-01-30 18:13:57 +0800

    basic version

--- guess_num.py
+++ guess_num.py
@@ -2,17 +2,13 @@ import random
 
 ans = random.randint(1, 100)
 
-count = 0
 while True:
-	count += 1
 	num = input('請輸入數字猜大小: ')
 	num = int(num)
 	if num == ans:
 		print('終於猜對了')
-		print('這是你猜的第', count, '次')
 		break
 	elif num > ans:
 			print('比答案大,往下猜')
 	elif num < ans:
 			print('比答案小,往上猜')
-	print('這是你猜的第', count, '次')
